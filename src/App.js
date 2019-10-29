7023de49b23261f3e8b2aa4b8313dd6d023c7987
rndpatel@gmail.com <rndpatel@gmail.com>
2019-10-29 14:11:03 -0700

    step 1 - axios call complete. button added to test call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            users: []
+        };
+    }
+
+    fetchUser = () => {
+      axios
+          .get('https://api.github.com/users/RaajnPatel')
+          .then(response => {
+              console.log(response);
+              this.setState({users: response.data});
+          })
+          .catch(err => console.log(err));
+    };
+
+    render() {
+     return(
+         <button onClick = {this.fetchUser}>Click Me</button>
+     )
+    }
 }
 
 export default App;
